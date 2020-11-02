4311ccd485d4707a139356806c956a6f574bb9cb
Jared Suresky <jsuresky@gmail.com>
2020-11-02 17:54:57 -0500

    third commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const data = [
+    id: 1, make: "Jeep", model: "Grand Cherokee", upvotes: 257},
+    id: 2, make: "Jeep", model: "Wrangler", upvotes: 400},
+    id: 3, make: "Jeep", model: "Renegade", upvotes: 32},
+    id: 4, make: "Jeep", model: "Gladiator", upvotes: 433},
+    id: 5, make: "Jeep", model: "Wagoneer", upvotes: 166},
+    id: 6, make: "Dodge",model: "Charger", upvotes: 744},
+    id: 7, make: "Dodge",model: "Challenger", upvotes: 722},
+    id: 8, make: "Plymouth", model: "Voyager", upvotes: 288},
+    id: 9, make: "Ram", model: "1500", upvotes: 544},
+    id: 10, make: "Chrysler", model: "Pacifica", upvotes: 206},
+]
