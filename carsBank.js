2707954e0300c21eb7d5fde34fa86442408f751e
Jared Suresky <jsuresky@gmail.com>
2020-11-04 18:35:16 -0500

    fifth commmit

--- carsBank.js
+++ carsBank.js
@@ -1,20 +1,19 @@
 const data = [
-    id: 1, make: "Jeep", model: "Grand Cherokee", upvotes: 257},
-    id: 2, make: "Jeep", model: "Wrangler", upvotes: 400},
-    id: 3, make: "Jeep", model: "Renegade", upvotes: 32},
-    id: 4, make: "Jeep", model: "Gladiator", upvotes: 433},
-    id: 5, make: "Jeep", model: "Wagoneer", upvotes: 166},
-    id: 6, make: "Dodge",model: "Charger", upvotes: 744},
-    id: 7, make: "Dodge",model: "Challenger", upvotes: 722},
-    id: 8, make: "Plymouth", model: "Voyager", upvotes: 288},
-    id: 9, make: "Ram", model: "1500", upvotes: 544},
-    id: 10, make: "Chrysler", model: "Pacifica", upvotes: 206},
-]
-const list = () {
-    return [...data] //returning a copy of the arrary
-};
-const find = (id) => {
-    const post = data.find(post => post.id === +id)
-};
+  { id: "1", make: "Jeep", model: "Wrangler", description: "Jeep Description" },
+  { id: "2", make: "Jeep", model: "Renegade", description: "Jeep Description" },
+  {
+    id: "3",
+    make: "Jeep",
+    model: "Gladiator",
+    description: "Jeep Description",
+  },
+  { id: "4", make: "Jeep", model: "Wagoneer", description: "Jeep Description" },
+  {
+    id: "5",
+    make: "Dodge",
+    model: "Charger",
+    description: "Dodge Description",
+  },
+];
 
-module.exports = {list: list, find: find}
+exports.data = data;
--- server.js
+++ server.js
@@ -1,24 +1,59 @@
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
-const carsBank = require("./carsBank.js");
+// const morgan = require("morgan");
+const carsBank = require("./carsBank");
 
-const carModels = require("./views/carsBank");
+// const list = () => {
+//   return [...data]; //returning a copy of the arrary
+// };
+// const find = (id) => {
+//   const post = data.find((post) => post.id === +id);
+// };
 
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  const cars = carsBank.list();
   const html = `<!DOCTYPE html>
     <html>
     <head>
-    <body>
-    <ul>
-    ${cars.map((car) => `<li> ${cars.model}</li>`)}
-    </ul>
-    </body>
     </head>
+    <h1>
+    Car Inventory
+    </h1>
+    <body>
+<div class="container1">
+${carsBank.data
+  .map(
+    (car) =>
+      `<div class="content"> <h3><a href=/car/${car.id}>${car.model}</a></h3></div>`
+  )
+  .join("")} 
+    </body>
     </html>`;
+  res.send(html);
+});
+
+app.get("/car/:id", (req, res) => {
+  //use carId as parameter and find it in the data using the function .find()
+  //create a variable such as consCar = carsBank.data.find()
+  ///camera/:cameraId"
+  const camera = data.cameras.find((c) => c.cameraId === req.params.cameraId);
+  const html = `<!DOCTYPE html>
+      <html>
+      <head>
+      </head>
+      <h1>
+      Car Inventory
+      </h1>
+      <body>
+  <div class="container1">
+ 
+      </body>
+      </html>`;
   res.send(html);
 });
 
